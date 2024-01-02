99a8189a29883852ce4988495e1beb5ef9991ce1
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-03 00:02:37 +0530

    First Commit

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/GarbageCollectionMsApplication.java
@@ -0,0 +1,13 @@
+package com.FinalYearProject.GarbageCollectionMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageCollectionMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbageCollectionMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FinalYearProject/GarbageCollectionMS/GarbageCollectionMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinalYearProject.GarbageCollectionMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageCollectionMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
