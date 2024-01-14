173b7f503e09b6d6f153f2128b12e0b9304ad204
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-14 23:18:44 +0530

    errors are fixed

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/entity/GarbageBin.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/entity/GarbageBin.java
@@ -1,18 +1,9 @@
 package com.FinalYearProject.GarbageCollectionMS.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-@Entity
 public class GarbageBin {
-    @Id
-    @GeneratedValue
+
     private int id;
     private float longitude;
     private float latitude;
-    private int houseHolderID;
-    private float fillLevel;
 
 }
