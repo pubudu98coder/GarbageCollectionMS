f36fa05200947d9e355fdf7976629a12d0029c5e
Madhara Chathurangi <142675042+madhara1020@users.noreply.github.com>
2024-01-17 15:59:08 +0530

    HouseHolder class created

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/HousholderRepo.java
@@ -0,0 +1,8 @@
+package com.FinalYearProject.GarbageCollectionMS.repo;
+
+import com.FinalYearProject.GarbageCollectionMS.entity.HouseHolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HousholderRepo extends JpaRepository<HouseHolder,Integer> {
+
+}
