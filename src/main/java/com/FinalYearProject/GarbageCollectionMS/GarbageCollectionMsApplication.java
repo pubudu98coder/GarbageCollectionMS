b6499e64426ec274fa74d21f7b3bc5fd067bce9b
Madhara Chathurangi <142675042+madhara1020@users.noreply.github.com>
2024-01-17 15:58:03 +0530

    modelmapper bean created

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/GarbageCollectionMsApplication.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/GarbageCollectionMsApplication.java
@@ -1,7 +1,9 @@
 package com.FinalYearProject.GarbageCollectionMS;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GarbageCollectionMsApplication {
@@ -9,5 +11,8 @@ public class GarbageCollectionMsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GarbageCollectionMsApplication.class, args);
 	}
-
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
