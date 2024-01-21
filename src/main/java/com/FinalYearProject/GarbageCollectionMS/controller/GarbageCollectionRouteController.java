791e92582d17b87bd6baa70052a1c8e2d090e590
sawan98 <lilankasawan@gmail.com>
2024-01-21 18:18:39 +0530

    Created the save vehicle function

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/GarbageCollectionRouteController.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/GarbageCollectionRouteController.java
@@ -1,19 +1,31 @@
 package com.FinalYearProject.GarbageCollectionMS.controller;
 
 import com.FinalYearProject.GarbageCollectionMS.dto.GarabageCollectionRouteDTO;
+import com.FinalYearProject.GarbageCollectionMS.dto.VehicleDTO;
 import com.FinalYearProject.GarbageCollectionMS.service.GarbageCollectionRouteService;
+import com.FinalYearProject.GarbageCollectionMS.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "api/v1/route")
+@RequestMapping("api/v1/route")
+@CrossOrigin
+
 public class GarbageCollectionRouteController {
     @Autowired
     private GarbageCollectionRouteService garabageCollectionRouteService;
+
+    @Autowired
+    private VehicleService vehicleService;
+
     @GetMapping(value = "/getRoute")
     public GarabageCollectionRouteDTO getRoute(){
         return garabageCollectionRouteService.Amain();
+    }
+
+    @PostMapping(value = "/saveVehicle")
+    public VehicleDTO saveVehicle(@RequestBody VehicleDTO vehicleDTO) {
+
+        return vehicleService.saveVehicle(vehicleDTO);
     }
 }
