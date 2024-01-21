ccda487fed6762ac498d9ed4eeef0c14f5650fe6
sawan98 <lilankasawan@gmail.com>
2024-01-22 01:24:09 +0530

    Removed the saveVehicle from GarbageCollectionRouteController

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/GarbageCollectionRouteController.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/GarbageCollectionRouteController.java
@@ -3,7 +3,6 @@ package com.FinalYearProject.GarbageCollectionMS.controller;
 import com.FinalYearProject.GarbageCollectionMS.dto.GarabageCollectionRouteDTO;
 import com.FinalYearProject.GarbageCollectionMS.dto.VehicleDTO;
 import com.FinalYearProject.GarbageCollectionMS.service.GarbageCollectionRouteService;
-import com.FinalYearProject.GarbageCollectionMS.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,17 +14,5 @@ public class GarbageCollectionRouteController {
     @Autowired
     private GarbageCollectionRouteService garabageCollectionRouteService;
 
-    @Autowired
-    private VehicleService vehicleService;
 
-    @GetMapping(value = "/getRoute")
-    public GarabageCollectionRouteDTO getRoute(){
-        return garabageCollectionRouteService.Amain();
-    }
-
-    @PostMapping(value = "/saveVehicle")
-    public VehicleDTO saveVehicle(@RequestBody VehicleDTO vehicleDTO) {
-
-        return vehicleService.saveVehicle(vehicleDTO);
-    }
 }
