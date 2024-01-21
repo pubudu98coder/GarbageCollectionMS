4d2dc7bcbbf5757ee3ca66bb6efb05e114c9a9d4
sawan98 <lilankasawan@gmail.com>
2024-01-22 01:25:05 +0530

    Created a separate controller to handle admin functions

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/AdminController.java
@@ -0,0 +1,22 @@
+package com.FinalYearProject.GarbageCollectionMS.controller;
+
+import com.FinalYearProject.GarbageCollectionMS.dto.VehicleDTO;
+import com.FinalYearProject.GarbageCollectionMS.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@CrossOrigin
+
+public class AdminController {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @PostMapping(value = "/saveVehicle")
+    public VehicleDTO saveVehicle(@RequestBody VehicleDTO vehicleDTO){
+
+        return vehicleService.saveVehicle(vehicleDTO);
+    }
+}
