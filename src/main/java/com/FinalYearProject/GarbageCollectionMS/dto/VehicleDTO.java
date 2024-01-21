ec5be1fd4c5f9408410dd04fb5c76418885c2b89
sawan98 <lilankasawan@gmail.com>
2024-01-21 18:41:42 +0530

    vehicleDTO and vehicleService created

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/dto/VehicleDTO.java
@@ -0,0 +1,15 @@
+package com.FinalYearProject.GarbageCollectionMS.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class VehicleDTO {
+    private int id;
+    private String RegNo;
+    private float capacity;
+}
