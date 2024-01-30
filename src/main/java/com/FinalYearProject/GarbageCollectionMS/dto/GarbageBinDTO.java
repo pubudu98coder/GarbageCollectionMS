b5f9ca0ae5e928cee4d99a7273f944072780efe6
sawan98 <lilankasawan@gmail.com>
2024-01-30 18:52:50 +0530

    created garbagebindto

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/dto/GarbageBinDTO.java
@@ -0,0 +1,19 @@
+package com.FinalYearProject.GarbageCollectionMS.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class GarbageBinDTO {
+
+    private float filledLevel;
+    private float filledWeight;
+    //private String status;
+
+
+
+}
