cbe0642cdecea36f310463caca72843077e9730c
sawan98 <lilankasawan@gmail.com>
2024-02-21 08:46:39 +0530

    Annotations for getters, setters and constructors were added

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/entity/Admin.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/entity/Admin.java
@@ -2,8 +2,17 @@ package com.FinalYearProject.GarbageCollectionMS.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 @DiscriminatorValue("admin")
 public class Admin extends User {
     private String empNumber;
