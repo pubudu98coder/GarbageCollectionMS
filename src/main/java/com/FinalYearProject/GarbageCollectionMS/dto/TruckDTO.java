34f3b419e1543e46f8342056e09a15a41d12129d
sawan98 <lilankasawan@gmail.com>
2024-02-22 21:45:59 +0530

    Created Truck DTO

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/dto/TruckDTO.java
@@ -0,0 +1,28 @@
+package com.FinalYearProject.GarbageCollectionMS.dto;
+
+import com.FinalYearProject.GarbageCollectionMS.entity.DriverTruckLog;
+import com.FinalYearProject.GarbageCollectionMS.entity.Route;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+
+public class TruckDTO {
+
+    @Column(nullable = false)
+    private String regNumber;
+
+    @Column(nullable = false)
+    private float capacity;
+
+    @Column(nullable = false)
+    private String status;
+
+    private List<Route> routes;
+
+    private List<DriverTruckLog> driverTruckLogList;
+
+}
