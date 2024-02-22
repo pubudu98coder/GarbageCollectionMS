42a22c65a17208543b423b98a2a69b9e309e1c1c
sawan98 <lilankasawan@gmail.com>
2024-02-22 21:56:31 +0530

    Created the function for adding a truck to the database

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/service/TruckService.java
@@ -0,0 +1,42 @@
+package com.FinalYearProject.GarbageCollectionMS.service;
+
+import com.FinalYearProject.GarbageCollectionMS.dto.GarbageBinDTO;
+import com.FinalYearProject.GarbageCollectionMS.dto.TruckDTO;
+import com.FinalYearProject.GarbageCollectionMS.entity.GarbageBin;
+import com.FinalYearProject.GarbageCollectionMS.entity.Truck;
+import com.FinalYearProject.GarbageCollectionMS.repo.TruckRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+
+public class TruckService {
+
+    @Autowired
+    private TruckRepo truckRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Truck addTruck(TruckDTO truckDTO){
+
+        Truck truck = new Truck();
+
+        truck.setRegNumber(truckDTO.getRegNumber());
+        truck.setCapacity(truckDTO.getCapacity());
+        truck.setStatus(truckDTO.getStatus());
+
+        return truckRepo.save(truck);
+
+    }
+
+
+
+
+
+
+}
