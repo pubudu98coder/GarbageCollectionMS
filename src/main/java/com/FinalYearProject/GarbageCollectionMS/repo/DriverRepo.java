4b48e4cc18fd8e368fb5d688e5a807a43857695b
sawan98 <lilankasawan@gmail.com>
2024-02-23 11:01:14 +0530

    Modified repository to find the available drivers

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/DriverRepo.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/DriverRepo.java
@@ -3,7 +3,9 @@ package com.FinalYearProject.GarbageCollectionMS.repo;
 import com.FinalYearProject.GarbageCollectionMS.entity.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DriverRepo extends JpaRepository<Driver,Integer> {
 
-
+    List<Driver> findByStatus(String status);
 }
