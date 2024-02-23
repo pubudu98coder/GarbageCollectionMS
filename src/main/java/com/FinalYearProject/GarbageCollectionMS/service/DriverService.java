6942b36eef1afa1e2fa074046efd2ecc474ecb64
sawan98 <lilankasawan@gmail.com>
2024-02-23 07:21:56 +0530

    Minor change in DTO name.

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/service/DriverService.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/service/DriverService.java
@@ -1,8 +1,7 @@
 package com.FinalYearProject.GarbageCollectionMS.service;
 
-import com.FinalYearProject.GarbageCollectionMS.dto.AddDriverDTO;
+import com.FinalYearProject.GarbageCollectionMS.dto.DriverDTO;
 import com.FinalYearProject.GarbageCollectionMS.entity.Driver;
-import com.FinalYearProject.GarbageCollectionMS.entity.Truck;
 import com.FinalYearProject.GarbageCollectionMS.repo.DriverRepo;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
@@ -20,7 +19,7 @@ public class DriverService {
     @Autowired
     private ModelMapper modelMapper;
 
-    public Driver addDriver(AddDriverDTO addDriverDTO){
+    public Driver addDriver(DriverDTO addDriverDTO){
 
         Driver driver = new Driver();
 
