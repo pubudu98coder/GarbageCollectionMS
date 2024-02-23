342fae8a5034e79f4e31164bc9c9894703f6fe2c
sawan98 <lilankasawan@gmail.com>
2024-02-23 07:14:24 +0530

    Created the function for adding a new driver to the system

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/service/DriverService.java
@@ -0,0 +1,42 @@
+package com.FinalYearProject.GarbageCollectionMS.service;
+
+import com.FinalYearProject.GarbageCollectionMS.dto.AddDriverDTO;
+import com.FinalYearProject.GarbageCollectionMS.entity.Driver;
+import com.FinalYearProject.GarbageCollectionMS.entity.Truck;
+import com.FinalYearProject.GarbageCollectionMS.repo.DriverRepo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+
+public class DriverService {
+
+    @Autowired
+    private DriverRepo driverRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Driver addDriver(AddDriverDTO addDriverDTO){
+
+        Driver driver = new Driver();
+
+        driver.setFirstName(addDriverDTO.getFirstName());
+        driver.setLastName(addDriverDTO.getLastName());
+        driver.setNicNumber(addDriverDTO.getNicNumber());
+        driver.setAddress(addDriverDTO.getAddress());
+        driver.setEmail(addDriverDTO.getEmail());
+        driver.setUserName(addDriverDTO.getUserName());
+        driver.setPassword(addDriverDTO.getPassword());
+        driver.setEmpNumber(addDriverDTO.getEmpNumber());
+        driver.setDlNumber(addDriverDTO.getDlNumber());
+
+
+
+        return driverRepo.save(driver);
+
+    }
+}
