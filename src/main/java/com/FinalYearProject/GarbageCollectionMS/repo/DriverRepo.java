9a2b4e6795fcd274ce4f4d2988e3fbbb28345097
sawan98 <lilankasawan@gmail.com>
2024-02-23 07:11:51 +0530

    Driver repository created

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/DriverRepo.java
@@ -0,0 +1,9 @@
+package com.FinalYearProject.GarbageCollectionMS.repo;
+
+import com.FinalYearProject.GarbageCollectionMS.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverRepo extends JpaRepository<Driver,Integer> {
+
+
+}
