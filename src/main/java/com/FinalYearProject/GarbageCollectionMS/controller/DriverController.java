cbf3036342ad80bcc080fadd65789b6fd45d27aa
sawan98 <lilankasawan@gmail.com>
2024-02-23 11:04:17 +0530

    Updated the controller to call the availableDrivers function

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/DriverController.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/DriverController.java
@@ -1,10 +1,13 @@
 package com.FinalYearProject.GarbageCollectionMS.controller;
 
 import com.FinalYearProject.GarbageCollectionMS.dto.DriverDTO;
+import com.FinalYearProject.GarbageCollectionMS.dto.TruckDTO;
 import com.FinalYearProject.GarbageCollectionMS.entity.Driver;
 import com.FinalYearProject.GarbageCollectionMS.service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/driver")
@@ -21,4 +24,17 @@ public class DriverController {
 
         return driverService.addDriver(addDriverDTO);
     }
+
+
+    @GetMapping(value = "/availableDrivers")
+    public List<DriverDTO> availableTrucks(){
+
+
+        return driverService.availableDrivers();
+    }
+
+
+
 }
+
+
