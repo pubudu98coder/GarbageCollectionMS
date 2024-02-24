d453099c2f4241a50af816cc808f0451786a3724
Madhara Chathurangi <142675042+madhara1020@users.noreply.github.com>
2024-02-24 21:16:26 +0530

    Create NewPageDTO class

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/dto/NewsPageDTO.java
@@ -0,0 +1,14 @@
+package com.FinalYearProject.GarbageCollectionMS.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class NewsPageDTO {
+    private int id;
+    private String newsCatergory;
+    private String newsDetails;
+}
