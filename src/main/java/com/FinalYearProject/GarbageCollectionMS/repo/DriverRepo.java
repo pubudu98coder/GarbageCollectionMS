74725302764cc1a107a4601267e647e094e779f6
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-02-26 09:44:33 +0530

    changed imports

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/DriverRepo.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/DriverRepo.java
@@ -1,6 +1,6 @@
 package com.FinalYearProject.GarbageCollectionMS.repo;
 
-import com.FinalYearProject.GarbageCollectionMS.entity.Driver;
+import com.FinalYearProject.GarbageCollectionMS.entity.users.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
