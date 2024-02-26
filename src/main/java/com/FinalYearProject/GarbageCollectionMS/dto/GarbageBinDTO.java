c4ae452630cc0097f3f1c9cbd4d8db26b5ac1332
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-02-26 09:46:34 +0530

    Add newBinDetails

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/dto/GarbageBinDTO.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/dto/GarbageBinDTO.java
@@ -1,5 +1,8 @@
 package com.FinalYearProject.GarbageCollectionMS.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +13,14 @@ import lombok.NoArgsConstructor;
 
 public class GarbageBinDTO {
 
-    private float filledLevel;
-    private float filledWeight;
+    private double height;
+
+    private String baseArea;
+
+    private int numOfTargetHouses;
+
+//    private float filledLevel;
+//    private float filledWeight;
     //private String status;
 
 
