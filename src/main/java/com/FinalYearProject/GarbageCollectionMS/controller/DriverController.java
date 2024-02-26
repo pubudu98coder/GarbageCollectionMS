102f0cae1734eae2ce6a8996c38bb4520fe680ad
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-02-26 09:45:00 +0530

    changed imports

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/DriverController.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/DriverController.java
@@ -1,8 +1,7 @@
 package com.FinalYearProject.GarbageCollectionMS.controller;
 
 import com.FinalYearProject.GarbageCollectionMS.dto.DriverDTO;
-import com.FinalYearProject.GarbageCollectionMS.dto.TruckDTO;
-import com.FinalYearProject.GarbageCollectionMS.entity.Driver;
+import com.FinalYearProject.GarbageCollectionMS.entity.users.Driver;
 import com.FinalYearProject.GarbageCollectionMS.service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
