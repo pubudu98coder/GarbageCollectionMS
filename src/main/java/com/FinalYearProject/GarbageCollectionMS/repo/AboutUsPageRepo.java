0b02704c2579f1553c5902a9f22e71d91081b501
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-02-27 10:32:17 +0530

    About us Repo

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/AboutUsPageRepo.java
@@ -0,0 +1,7 @@
+package com.FinalYearProject.GarbageCollectionMS.repo;
+
+import com.FinalYearProject.GarbageCollectionMS.entity.AboutUsPage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AboutUsPageRepo extends JpaRepository<AboutUsPage,Integer> {
+}
