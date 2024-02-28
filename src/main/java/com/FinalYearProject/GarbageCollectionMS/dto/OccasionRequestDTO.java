0f6be1d4fbf350fe2cd56b217049f61cab892e13
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-02-28 17:26:15 +0530

    create occasion request dto

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/dto/OccasionRequestDTO.java
@@ -0,0 +1,18 @@
+package com.FinalYearProject.GarbageCollectionMS.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OccasionRequestDTO {
+
+    private int id;
+    private String occasionType;
+    private Date date;
+    private String contactNumber;
+    private  String description;
+}
