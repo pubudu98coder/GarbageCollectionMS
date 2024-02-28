2b47d194c9ff46701684ca0a8b9c77f44aae7823
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-02-28 17:29:16 +0530

    add house owner complaints

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/HouseOwnerComplaintsController.java
@@ -0,0 +1,24 @@
+package com.FinalYearProject.GarbageCollectionMS.controller;
+
+
+import com.FinalYearProject.GarbageCollectionMS.dto.DriverDTO;
+import com.FinalYearProject.GarbageCollectionMS.dto.HouseOwnerComplaintsDTO;
+import com.FinalYearProject.GarbageCollectionMS.entity.HouseOwnerComplaints;
+import com.FinalYearProject.GarbageCollectionMS.entity.users.Driver;
+import com.FinalYearProject.GarbageCollectionMS.service.HouseOwnerComplaintsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/v1/auth/HouseOwnerComplaints")
+@CrossOrigin
+public class HouseOwnerComplaintsController {
+    @Autowired
+    private HouseOwnerComplaintsService houseOwnerComplaintsService;
+
+    @PostMapping(value = "/addComplaints")
+    public HouseOwnerComplaints addHouseOwnerComplaints(@RequestBody HouseOwnerComplaintsDTO addHouseOwnerComplaintsDTO){
+
+        return houseOwnerComplaintsService.addHouseOwnerComplaints(addHouseOwnerComplaintsDTO);
+    }
+}
