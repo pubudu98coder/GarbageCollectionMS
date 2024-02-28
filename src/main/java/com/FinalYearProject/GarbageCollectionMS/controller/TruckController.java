fe148ff1fccfff860b069a23e9d810e7700c518f
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-02-28 17:23:49 +0530

    change path of request mapping

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/TruckController.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/controller/TruckController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/v1/truck")
+@RequestMapping(value = "/api/v1/auth/truck")
 @CrossOrigin
 
 public class TruckController {
