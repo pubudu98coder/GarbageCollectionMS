0b07b70d78e63092944192602b779092dea24425
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-02-28 17:21:33 +0530

    created supervisor class and repo

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/SupervisorRepository.java
@@ -0,0 +1,8 @@
+package com.FinalYearProject.GarbageCollectionMS.repo;
+
+import com.FinalYearProject.GarbageCollectionMS.entity.users.Supervisor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupervisorRepository extends JpaRepository<Supervisor,Integer> {
+
+}
