e7a1a204c1c9863aa97fc2537ebf23d8cd881012
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-04-15 09:32:42 +0530

    created DTO for get iot inputs

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/dto/GarbageBinIOTInput.java
@@ -0,0 +1,10 @@
+package com.FinalYearProject.GarbageCollectionMS.dto;
+
+import lombok.Data;
+
+@Data
+public class GarbageBinIOTInput {
+    private double filledHeight;
+    private double longitude;
+    private double latitude;
+}
