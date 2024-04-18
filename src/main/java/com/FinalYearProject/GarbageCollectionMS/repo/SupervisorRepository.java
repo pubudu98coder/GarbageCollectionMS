5dec406f10351bf59f772e5339ed7c543b0e87e3
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-04-18 14:24:58 +0530

    moved

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/SupervisorRepository.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/SupervisorRepository.java
@@ -1,6 +1,6 @@
 package com.FinalYearProject.GarbageCollectionMS.repo;
 
-import com.FinalYearProject.GarbageCollectionMS.entity.users.Visible.Supervisor;
+import com.FinalYearProject.GarbageCollectionMS.entity.users.Supervisor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SupervisorRepository extends JpaRepository<Supervisor,Integer> {
