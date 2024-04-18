0c2d48f8c384d7ae36f1ab0bdd4ec2a0e097b09f
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-04-18 14:22:10 +0530

    commented supervisor Permissions

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/entity/users/Permission.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/entity/users/Permission.java
@@ -10,14 +10,17 @@ public enum Permission {
     ADMIN_UPDATE("admin:update"),
     ADMIN_CREATE("admin:create"),
     ADMIN_DELETE("admin:delete"),
-    SUPERVISOR_READ("management:read"),
-    SUPERVISOR_UPDATE("management:update"),
-    SUPERVISOR_CREATE("management:create"),
-    SUPERVISOR_DELETE("management:delete"),
+
+//    SUPERVISOR_READ("management:read"),
+//    SUPERVISOR_UPDATE("management:update"),
+//    SUPERVISOR_CREATE("management:create"),
+//    SUPERVISOR_DELETE("management:delete"),
+
     HOUSEHOLDER_READ("house_holder:read"),
     HOUSEHOLDER_UPDATE("house_holder:update"),
     HOUSEHOLDER_CREATE("house_holder:create"),
     HOUSEHOLDER_DELETE("house_holder:delete"),
+
     DRIVER_READ("truck_driver:read"),
     DRIVER_UPDATE("truck_driver:update"),
     DRIVER_CREATE("truck_driver:create"),
