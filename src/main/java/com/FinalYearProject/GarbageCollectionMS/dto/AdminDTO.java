a1e2f0a0a14f4bb64e17d2f48af27841d71db9e0
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-04-19 15:36:30 +0530

    created AdminDTO

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/dto/AdminDTO.java
@@ -0,0 +1,18 @@
+package com.FinalYearProject.GarbageCollectionMS.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AdminDTO {
+    private String firstName;
+    private String lastName;
+    private String nicNo;
+    private String address;
+    private String email;
+    private String mobileNo;
+    private String adNo;
+    private String password;
+}
