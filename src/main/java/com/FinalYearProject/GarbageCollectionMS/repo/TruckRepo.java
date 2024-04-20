3971c38b5c51cfc94aad5bad467d7657fd4c98cc
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-04-20 21:48:00 +0530

    modified the truckrepo

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/TruckRepo.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/repo/TruckRepo.java
@@ -8,9 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TruckRepo extends JpaRepository<Truck,Integer> {
-
-
    List<Truck> findByStatus(String status);
+
+   boolean existsByRegNumber(String regNumber);
 
 
 
