b93676d6c5ee3cba1d525ca86f111b488b7acdfb
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-04-20 21:46:23 +0530

    changed TruckDTO

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/dto/TruckDTO.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/dto/TruckDTO.java
@@ -2,6 +2,7 @@ package com.FinalYearProject.GarbageCollectionMS.dto;
 
 import com.FinalYearProject.GarbageCollectionMS.entity.DriverTruckLog;
 import com.FinalYearProject.GarbageCollectionMS.entity.Route;
+import com.FinalYearProject.GarbageCollectionMS.util.TruckType;
 import jakarta.persistence.Column;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
@@ -9,20 +10,17 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-
 public class TruckDTO {
-
-    @Column(nullable = false)
     private String regNumber;
 
-    @Column(nullable = false)
     private float capacity;
 
-    @Column(nullable = false)
     private String status;
 
-    private List<Route> routes;
+    private TruckType type;
 
-    private List<DriverTruckLog> driverTruckLogList;
+//    private List<Route> routes;
+//
+//    private List<DriverTruckLog> driverTruckLogList;
 
 }
