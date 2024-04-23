da0f5a8e83f472d6989f4efc3635ba42e88c9ac5
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-04-23 14:21:28 +0530

    created DAO s for getting driver data

---
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/dao/DriverDAO.java
@@ -0,0 +1,17 @@
+package com.FinalYearProject.GarbageCollectionMS.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DriverDAO {
+
+    private String empNumber;
+    private String firstName;
+    private String status;
+    private String nicNo;
+
+}
