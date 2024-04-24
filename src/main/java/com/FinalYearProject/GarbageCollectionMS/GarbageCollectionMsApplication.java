217eb19bf7fe0b3ccd67175817596d1a7b92f5b2
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-04-24 16:58:08 +0530

    small changes

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/GarbageCollectionMsApplication.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/GarbageCollectionMsApplication.java
@@ -2,24 +2,21 @@ package com.FinalYearProject.GarbageCollectionMS;
 
 import com.FinalYearProject.GarbageCollectionMS.auth.AuthenticationService;
 import com.FinalYearProject.GarbageCollectionMS.auth.RegisterRequest;
-import com.FinalYearProject.GarbageCollectionMS.dto.AboutUsPageDTO;
-import com.FinalYearProject.GarbageCollectionMS.dto.GarbageBinDTO;
-import com.FinalYearProject.GarbageCollectionMS.dto.NewsPageDTO;
-import com.FinalYearProject.GarbageCollectionMS.dto.OccasionRequestDTO;
+import com.FinalYearProject.GarbageCollectionMS.dto.*;
 import com.FinalYearProject.GarbageCollectionMS.entity.users.Visible.HouseHolder;
 import com.FinalYearProject.GarbageCollectionMS.entity.users.User;
 import com.FinalYearProject.GarbageCollectionMS.repo.HouseHolderRepository;
 import com.FinalYearProject.GarbageCollectionMS.repo.UserRepository;
 import com.FinalYearProject.GarbageCollectionMS.entity.users.Role;
-import com.FinalYearProject.GarbageCollectionMS.service.AboutUsPageService;
-import com.FinalYearProject.GarbageCollectionMS.service.GarbageBinService;
-import com.FinalYearProject.GarbageCollectionMS.service.HouseOwnerComplaintsService;
-import com.FinalYearProject.GarbageCollectionMS.service.NewsPageService;
+import com.FinalYearProject.GarbageCollectionMS.service.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @SpringBootApplication
 public class GarbageCollectionMsApplication {
@@ -35,7 +32,7 @@ public class GarbageCollectionMsApplication {
 	}
 	@Bean
 	public CommandLineRunner commandLineRunner(
-			AuthenticationService service, HouseHolderRepository houseHolderRepository, UserRepository userRepository,GarbageBinService garbageBinService, AboutUsPageService aboutUsPageService, HouseOwnerComplaintsService houseOwnerComplaintsService, NewsPageService newsPageService
+			AuthenticationService service, HouseHolderRepository houseHolderRepository, UserRepository userRepository,GarbageBinService garbageBinService, AboutUsPageService aboutUsPageService, HouseOwnerComplaintsService houseOwnerComplaintsService, NewsPageService newsPageService, OccasionRequestService occasionRequestService
 	) {
 		return args -> {
 			var admin = RegisterRequest.builder()
@@ -48,6 +45,16 @@ public class GarbageCollectionMsApplication {
 					.build();
 			System.out.println("Admin token: " + service.register(admin).getAccessToken());
 
+			var driver = RegisterRequest.builder()
+					.firstName("Ama")
+					.lastName("Nirupadi")
+					.email("ama@gmail.com")
+					.password("12345")
+					.nicNo("200059470838")
+					.role(Role.DRIVER)
+					.build();
+			System.out.println("Driver token: " + service.register(driver).getAccessToken());
+
 			var houseHolder = RegisterRequest.builder()
 					.firstName("Lilanka")
 					.lastName("Sawan")
@@ -57,13 +64,13 @@ public class GarbageCollectionMsApplication {
 					.role(Role.HOUSEHOLDER)
 					.build();
 			System.out.println("HouseHolder token: " + service.register(houseHolder).getAccessToken());
-			HouseHolder houseHolder1=houseHolderRepository.findById(2).orElse(null);
-			houseHolder1.setHouseNo("h1");
-			houseHolderRepository.save(houseHolder1);
-			User adminG=userRepository.findById(1).orElse(null);
-			HouseHolder houseHolder2=houseHolderRepository.findByHouseNo("h1").orElse(null);
-			System.out.println(houseHolder2.getFirstName());
-			System.out.println(adminG.getFirstName());
+//			HouseHolder houseHolder1=houseHolderRepository.findById(3).orElse(null);
+//			houseHolder1.setHouseNo("h1");
+//			houseHolderRepository.save(houseHolder1);
+//			User adminG=userRepository.findById(1).orElse(null);
+//			HouseHolder houseHolder2=houseHolderRepository.findByHouseNo("h1").orElse(null);
+//			System.out.println(houseHolder2.getFirstName());
+//			System.out.println(adminG.getFirstName());
 
 			//added from ama
 //			GarbageBinDTO garbageBinDTO=new GarbageBinDTO();
@@ -87,7 +94,18 @@ public class GarbageCollectionMsApplication {
 			newsPageDTO.setSelectedImage("f.jpg");
 			newsPageService.addNewsPageData(newsPageDTO);
 
+			HouseOwnerComplaintsDTO houseOwnerComplaintsDTO = new HouseOwnerComplaintsDTO();
+			houseOwnerComplaintsDTO.setComplaintType("ffgd");
+			houseOwnerComplaintsDTO.setDescription("ggfgf");
+			houseOwnerComplaintsDTO.setContactNo("0776026233");
+			houseOwnerComplaintsService.addHouseOwnerComplaints(houseOwnerComplaintsDTO);
 
+			OccasionRequestDTO occasionRequestDTO = new OccasionRequestDTO();
+			occasionRequestDTO.setOccasionType("rrgr");
+			occasionRequestDTO.setDate("2024-04-05");
+			occasionRequestDTO.setContactNumber("0776026233");
+			occasionRequestDTO.setDescription("ghuh");
+			occasionRequestService.addOccasionRequest(occasionRequestDTO);
 
 			//added from ama
 //			AboutUsPageDTO aboutUsPageDTO=new AboutUsPageDTO();
