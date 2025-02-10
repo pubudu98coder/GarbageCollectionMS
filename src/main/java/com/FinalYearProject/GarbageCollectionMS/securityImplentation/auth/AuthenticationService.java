5844350cbfa4d7d2a51c9071678a9fad065f5c60
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2025-02-10 09:21:59 +0530

    refactoring-removed the single endpoint for registering all users

--- src/main/java/com/FinalYearProject/GarbageCollectionMS/securityImplentation/auth/AuthenticationService.java
+++ src/main/java/com/FinalYearProject/GarbageCollectionMS/securityImplentation/auth/AuthenticationService.java
@@ -1,10 +1,11 @@
 package com.FinalYearProject.GarbageCollectionMS.securityImplentation.auth;
 
+import com.FinalYearProject.GarbageCollectionMS.dto.Driver.DriverRegisterRequestDTO;
+import com.FinalYearProject.GarbageCollectionMS.mapper.DriverMapper;
 import com.FinalYearProject.GarbageCollectionMS.securityImplentation.config.Role;
 import com.FinalYearProject.GarbageCollectionMS.securityImplentation.User;
 import com.FinalYearProject.GarbageCollectionMS.securityImplentation.config.JwtService;
 import com.FinalYearProject.GarbageCollectionMS.entity.users.Driver;
-import com.FinalYearProject.GarbageCollectionMS.entity.users.HouseHolder;
 import com.FinalYearProject.GarbageCollectionMS.Repository.*;
 import com.FinalYearProject.GarbageCollectionMS.securityImplentation.token.Token;
 import com.FinalYearProject.GarbageCollectionMS.securityImplentation.token.TokenType;
@@ -12,6 +13,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -27,55 +29,49 @@ public class AuthenticationService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
     private final TokenRepository tokenRepository;
-    private final HouseHolderRepository houseHolderRepository;
     private final DriverRepository driverRepository;
+    private final DriverMapper driverMapper;
 
     public AuthenticationResponse register(RegisterRequest request){
-        User user=null;
-
-        if(request.getRole()== Role.HOUSEHOLDER){
-            user=new HouseHolder();
-            user.setFirstName(request.getFirstName());
-            user.setLastName(request.getLastName());
-            user.setEmail(request.getEmail());
-            user.setNicNo(request.getNicNo());
-            user.setPassword(passwordEncoder.encode(request.getPassword()));
-            user.setRole(request.getRole());
-
-            houseHolderRepository.save((HouseHolder) user);
-        }
-        if(request.getRole()==Role.ADMIN){
-            user=new User();
-            user.setFirstName(request.getFirstName());
-            user.setLastName(request.getLastName());
-            user.setEmail(request.getEmail());
-            user.setNicNo(request.getNicNo());
-            user.setPassword(passwordEncoder.encode(request.getPassword()));
-            user.setRole(request.getRole());
-
-
-            userRepository.save(user);
-        }
-        if(request.getRole()==Role.DRIVER){
-            user=new Driver();
-            user.setFirstName(request.getFirstName());
-            user.setLastName(request.getLastName());
-            user.setEmail(request.getEmail());
-            user.setNicNo(request.getNicNo());
-            user.setPassword(passwordEncoder.encode(request.getPassword()));
-            user.setRole(request.getRole());
-
-
-            driverRepository.save((Driver) user);
-        }
-
-        //userRepository.save(user);
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .nicNo(request.getNicNo())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.ADMIN)
+                .mobileNo(request.getMobileNo())
+                .build();
+        userRepository.save(user);
         var jwtToken=jwtService.generateToken(user);
         var refreshToken=jwtService.generateRefreshToken(user);
         revokeAllUserTokens(user);
         saveUserToken(user,jwtToken);
         return AuthenticationResponse.builder().accessToken(jwtToken).refreshToken(refreshToken).role(user.getRole()).build();
     }
+
+    public AuthenticationResponse registerDriver(DriverRegisterRequestDTO driverRegisterRequestDTO){
+        if (userRepository.existsByNicNo(driverRegisterRequestDTO.nicNo())){
+            throw new DataIntegrityViolationException("Driver NIC number already Exists");
+        } else {
+            Driver driver = driverMapper.toDriver(driverRegisterRequestDTO);
+            driver.setPassword(passwordEncoder.encode(driverRegisterRequestDTO.password()));
+            driver.setRole(Role.DRIVER);
+            driverRepository.save(driver);
+
+            var jwtToken = jwtService.generateToken(driver);
+            var refreshToken = jwtService.generateRefreshToken((driver));
+            revokeAllUserTokens(driver);
+            saveUserToken(driver, jwtToken);
+
+            return AuthenticationResponse.builder()
+                    .accessToken(jwtToken)
+                    .refreshToken(refreshToken)
+                    .role(Role.HOUSEHOLDER)
+                    .build();
+        }
+    }
+
     public AuthenticationResponse authenticate(AuthenticationRequest request){
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
@@ -114,34 +110,11 @@ public class AuthenticationService {
         tokenRepository.saveAll(validUserTokens);
     }
 
-//    public void refreshToken(
-//            HttpServletRequest request,
-//            HttpServletResponse response) throws IOException {
-//        final String authHeader=request.getHeader(HttpHeaders.AUTHORIZATION);
-//        final String refreshToken;
-//        final String nicNo;
-//        if(authHeader==null||!authHeader.startsWith("Bearer ")){
-//            return;
-//        }
-//        refreshToken=authHeader.substring(7);
-//        nicNo=jwtService.extractUsername(refreshToken);
-//        if(nicNo!=null){
-//            var user=this.userRepository.findByNicNo(nicNo).orElseThrow();
-//            if(jwtService.isTokenValid(refreshToken,user)){
-//                var accessToken= jwtService.generateToken(user);
-//                revokeAllUserTokens(user);
-//                saveUserToken(user,accessToken);
-//                var authResponse=AuthenticationResponse.builder()
-//                        .accessToken(accessToken)
-//                        .refreshToken(refreshToken)
-//                        .build();
-//                new ObjectMapper().writeValue(response.getOutputStream(),authResponse);
-//            }
-//        }
-//    }
 public void refreshToken(
         HttpServletRequest request,
-        HttpServletResponse response) throws IOException {
+        HttpServletResponse response
+
+) throws IOException {
     final String authHeader=request.getHeader(HttpHeaders.AUTHORIZATION);
     final String refreshToken;
     final String nicNo;
